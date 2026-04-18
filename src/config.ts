af1355ba0b6e8e545bc8f387d9c4404576447c80
lijiaxu2021 <lijiaxulove@outlook.com>
2026-04-18 13:00:14 +0800

    修改 GitHub 链接为 ImUpxuu，修复图库横向排序

--- src/config.ts
+++ src/config.ts
@@ -89,7 +89,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/lijiaxu2021", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/ImUpxuu", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -103,7 +103,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/lijiaxu2021",
+			url: "https://github.com/ImUpxuu",
 		},
 	],
 };
