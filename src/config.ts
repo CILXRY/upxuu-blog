f51e42040fd39334cf83231d7d06a2a42d7473a7
lijiaxu2021 <lijiaxulove@outlook.com>
2026-04-26 20:47:20 +0800

    fix: move Q群 to top-level navbar

--- src/config.ts
+++ src/config.ts
@@ -73,12 +73,12 @@ export const navBarConfig: NavBarConfig = {
 					url: "https://stats.upxuu.com/share/sFftlqBkgk2z9JM2",
 					external: true,
 				},
-				{
-					name: "QQ群",
-					url: "/q",
-					external: false,
-				},
 			],
+		},
+		{
+			name: "Q群",
+			url: "/q",
+			external: false,
 		},
 		{
 			name: "开往",
